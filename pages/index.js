7926a39b5083c965706b803c8f7ea3f8d7d2b943
Ajay Upadhyay <ajayupadhyay@pop-os.localdomain>
2022-01-21 21:07:11 +0530

    index.js change

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Base from "../src/Base/Base";
 import Blob from "../public/blob.svg";
 import { GiFluffyWing } from "react-icons/gi";
 export default function Home() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Head>
